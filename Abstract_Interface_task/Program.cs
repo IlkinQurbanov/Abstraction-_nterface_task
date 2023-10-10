9bb7e6cd03984e4fdeba6d255d6356d118a42b16
IlkinQurbanov <ilkin.qurbanli.92@inbox.ru>
2023-10-10 09:10:12 +0400

    Task

---
+++ Abstract_Interface_task/Program.cs
@@ -0,0 +1,22 @@
+﻿using Abstract_Interface_task;
+
+
+static void Main(string[] args)
+{
+    string userName = Console.ReadLine();
+    string password = Console.ReadLine();
+
+    User user = new User(userName, password);
+
+    Console.WriteLine("Istifadecinin adi: {0}", user.UserName);
+    Console.WriteLine("Parol: {0}", user.Password);
+
+    bool hasDigit = user.HasDigit(password);
+    Console.WriteLine("Parol reqemlerden ibaret: {0}", hasDigit);
+
+    bool hasUpper = user.HasUpper(password);
+    Console.WriteLine("Parol böyük hərfdən ibarətdir: {0}", hasUpper);
+
+    bool hasLower = user.HasLower(password);
+    Console.WriteLine("Parol kiçik hərfdən ibarətdir: {0}", hasLower);
+}
---
+++ Abstract_Interface_task/User.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Interface_task
+{
+    public class User
+    {
+        private string userName;
+        private string password;
+
+        public User(string userName, string password)
+        {
+            if (string.IsNullOrEmpty(userName))
+            {
+                throw new ArgumentException("Ad bosh qala bilmez.");
+            }
+
+            if (userName.Length < 6 || userName.Length > 25)
+            {
+                throw new ArgumentException("Ad minimum 25 simvol olmalidir.");
+            }
+
+            if (password.Length < 8 || password.Length > 25)
+            {
+                throw new ArgumentException("Parol 8 simvoldan 25 e qeder olmalidir.");
+            }
+
+            if (!HasUpper(password) || !HasLower(password) || !HasDigit(password))
+            {
+                throw new ArgumentException("Parolda ən azı bir böyük hərf, bir kiçik hərf və bir rəqəm olmalıdır.");
+            }
+
+            this.userName = userName;
+            this.password = password;
+        }
+
+        public string UserName
+        {
+            get { return this.userName; }
+        }
+
+        public string Password
+        {
+            get { return this.password; }
+        }
+
+        public bool HasDigit(string value)
+        {
+            return value.Any(c => Char.IsDigit(c));
+        }
+
+        public bool HasUpper(string value)
+        {
+            return value.Any(c => Char.IsUpper(c));
+        }
+
+        public bool HasLower(string value)
+        {
+            return value.Any(c => Char.IsLower(c));
+        }
+    }
+
+}
